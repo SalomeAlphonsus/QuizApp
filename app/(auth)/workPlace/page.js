abd41bb8e28344bcfd968d99c41ae4637fa9701e
SalomeAlphonsus <ikpegbusalome3@gmail.com>
2025-07-11 13:45:45 +0100

    removed suspense

--- app/(auth)/workPlace/page.js
+++ app/(auth)/workPlace/page.js
@@ -1,14 +1,11 @@
 'use client'
-import { Suspense } from "react";
 
 import WorkPlaceForm from "./workPlaceForm";
 
 
 export default function Page() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
            <WorkPlaceForm/>
-        </Suspense>
             
         
     )
